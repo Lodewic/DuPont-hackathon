044cb7794ebf3044943b65e505e947d356e822a8
shamanou <svanleeuwen@qualogy.com>
2019-06-04 15:42:16 +0200

    updates

--- model.py
+++ model.py
@@ -2,27 +2,30 @@ from keras.models import Sequential
 from keras import preprocessing
 import sys
 import numpy as np
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, LSTM,Embedding
 
-import_file = open(sys.argv[1],'r').readlines()
+import_file = open(sys.argv[1],'r').read().strip('\n').split('\n')
 
-def one_hot_encode(sequence: list or str, encoding: dict, max_len: int = None) -> np.array:
-    one_hot_sequence = np.array([encoding[element] for element in sequence])
-    if max_len is not None:
-        padding = np.zeros(shape=(max_len - len(sequence), len(encoding[sequence[0]])))
-        one_hot_sequence = np.concatenate([one_hot_sequence, padding])
-    return one_hot_sequence.tolist()
+def one_hot_encode(sequence,encoding):
+    return [ [ 1 if x == aa else 0 for aa in encoding ] for x in sequence ]
 
 #encode
+
+AA_LABELS = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S',
+             'T', 'V', 'W', 'Y']
+
 list_input = []
 for line in import_file:
-	list_input.append(one_hot_encode(line, 26))
+	list_input.append(one_hot_encode(line.strip(), AA_LABELS))
 
 n_list = np.array(list_input, "float32")
 
 model = Sequential()
-model.add(Dense(32, input_shape=n_list.shape[:1]))
+model.add(Embedding(len(AA_LABELS)+1, 100,input_length=(n_list.shape[1],n_list.shape[2])))
 model.add(LSTM(15))
 model.add(Dense(3))
-model.compile('SGD')
-model.fit()
+model.compile(optimizer='rmsprop',
+              loss='mse',
+              metrics=['accuracy'])
+model.fit(n_list, epochs=10, batch_size=32)
+
